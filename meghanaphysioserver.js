f3b36ff26044943cd2739c5b5aef57aeb6e79582
muni6864 <54971695+muni6864@users.noreply.github.com>
2026-01-26 08:39:19 -0500

    Update meghanaphysioserver.js

--- meghanaphysioserver.js
+++ meghanaphysioserver.js
@@ -1555,6 +1555,11 @@ function renderHTML(doctor, feedbacks, stats) {
   <div class="vid-info"><span class="vid-title">Lower Back Pain</span></div>
   <div class="play-indicator">${PLAY_ICON}</div>
 </button>
+
+<button class="video-btn" onclick="openVideo('https://www.youtube.com/embed/v5SlZz3E8tc')">
+  <div class="vid-info"><span class="vid-title">Hip Pain</span></div>
+  <div class="play-indicator">${PLAY_ICON}</div>
+</button>
 </div>
 
 </div>
@@ -1784,6 +1789,7 @@ app.listen(PORT, () => {
 });
 
  
+
 
 
 
